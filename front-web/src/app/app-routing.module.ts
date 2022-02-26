3eae22731fdde2c9d14a5058c997783925c93913
Lucas de Mello <lucas_mello6@hotmail.com>
2022-02-25 23:21:40 -0300

    configure routes

--- front-web/src/app/app-routing.module.ts
+++ front-web/src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { OrdersComponent } from './pages/orders/orders.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'orders',
+    component: OrdersComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
